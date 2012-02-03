ffe506b1ea38e11715baf4f0fc74c5525ee059cf
Christian Zachariasen <chrizach@gmail.com>
2012-02-03 13:52:05 +0100

    Added missing files

---
+++ Profiler/IDbProfiler.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace Monocle.Profiler
+{
+    public interface IDbProfiler
+    {
+        void StartProfiling(SqlCommand command);
+        void EndProfiling();
+        DbProfilingResults Results { get; }
+    }
+}
